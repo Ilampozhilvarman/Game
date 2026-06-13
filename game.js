860fd076ce832d8f95d1127e6f3bfdd0e5cdcd35
Ilampozhilvarman <Ilampozhilvarman@gmail.com>
2026-06-13 09:27:15 -0500

    Changes happend.

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const config = {
      physics: {
          default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { x: 0, y: 300 },
             debug: false
         }
     },
@@ -27,16 +27,14 @@ function preload() {
 }
 
 function create() {
-    let square = this.add.rectangle(400, 300, 50, 50, 0x00ff00);
-    this.physics.add.existing(square);
-    square.body.setCollideWorldBounds(true);
-    square.body.setBounce(0.2);
-    square.body.setVelocity(200, 150);
+    this.square = this.add.rectangle(400, 300, 50, 50, 0x00ff00);
+    this.physics.add.existing(this.square);
+    this.square.body.setCollideWorldBounds(true);
     this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 }
 
 function update() {
-    if (this.spacebar.isDown) {
-        square.body.setVelocityY(-300);
+    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+        this.square.body.setVelocityY(-250);
     }
 }
